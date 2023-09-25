b07e83f7cc1cd0bc6ffbbeacd3e2723a25946ca2
Ahmed Taha <ahmdtaha@umd.edu>
2023-09-25 11:57:39 -0700

    Automate DDPSigmoidLoss test using assert

--- test_distributed_sigmoid_loss.py
+++ test_distributed_sigmoid_loss.py
@@ -82,12 +82,11 @@ def average_gradients(model):
         param.grad.data /= size
 
 
-def toy_forward_backward_pass(rank, world_size, bz):
+def toy_forward_backward_pass(rank, world_size, bz, emb_dim=2, return_dict=None):
 
     setup(rank, world_size)
     assert bz % world_size == 0
     gpu_batch_size = bz // world_size
-    emb_dim = 2
 
     image_inputs, text_inputs = get_partition(rank, world_size, gpu_batch_size, emb_dim)
     image_encoder, text_encoder = get_encoders(emb_dim)
@@ -104,18 +103,68 @@ def toy_forward_backward_pass(rank, world_size, bz):
 
     average_gradients(text_encoder)
     # # # check gradients
-    print(f"Rank:{rank} text_encoder.weight.grad: {text_encoder.weight.grad}")
+    # print(f"Rank:{rank} text_encoder.weight.grad: {text_encoder.weight.grad}")
 
     # # # average gradient from all devices
     average_gradients(image_encoder)
     # # # check gradients
-    print(f"Rank:{rank} image_encoder.weight.grad: {image_encoder.weight.grad}")
+    # print(f"Rank:{rank} image_encoder.weight.grad: {image_encoder.weight.grad}")
+    if rank == 0:
+        return_dict['img_grad'] = image_encoder.weight.grad
+        return_dict['txt_grad'] = text_encoder.weight.grad
 
 
-def run_demo(demo_fn, world_size=3, batch_size=3):
-    mp.spawn(demo_fn, args=(world_size, batch_size), nprocs=world_size, join=True)
+def test_odd_world_size(emb_dim=2):
+    manager = mp.Manager()
+    world_size, batch_size = 3, 3
+    w3_return_dict = manager.dict()
+    mp.spawn(
+        toy_forward_backward_pass,
+        args=(world_size, batch_size, emb_dim, w3_return_dict),
+        nprocs=world_size,
+        join=True,
+    )
+
+    w1_return_dict = manager.dict()
+    world_size, batch_size = 1, 3
+    mp.spawn(
+        toy_forward_backward_pass,
+        args=(world_size, batch_size, emb_dim, w1_return_dict),
+        nprocs=world_size,
+        join=True,
+    )
+
+    assert torch.allclose(w3_return_dict['img_grad'], w1_return_dict['img_grad'])
+    assert torch.allclose(w3_return_dict['txt_grad'], w1_return_dict['txt_grad'])
+
+
+def test_even_world_size(emb_dim=2):
+    manager = mp.Manager()
+    world_size, batch_size = 2, 4
+    w2_return_dict = manager.dict()
+    mp.spawn(
+        toy_forward_backward_pass,
+        args=(world_size, batch_size, emb_dim, w2_return_dict),
+        nprocs=world_size,
+        join=True,
+    )
+
+    w1_return_dict = manager.dict()
+    world_size, batch_size = 1, 4
+    mp.spawn(
+        toy_forward_backward_pass,
+        args=(world_size, batch_size, emb_dim, w1_return_dict),
+        nprocs=world_size,
+        join=True,
+    )
+    # print(w2_return_dict['img_grad'])
+    # print(w1_return_dict['img_grad'])
+    assert torch.allclose(w2_return_dict['img_grad'], w1_return_dict['img_grad'], rtol=1e-4)
+    assert torch.allclose(w2_return_dict['txt_grad'], w1_return_dict['txt_grad'], rtol=1e-4)
 
 
 if __name__ == "__main__":
-    run_demo(toy_forward_backward_pass, world_size=3, batch_size=3)
-    run_demo(toy_forward_backward_pass, world_size=1, batch_size=3)
+    # test_odd_world_size()
+    # test_even_world_size()
+    test_even_world_size(emb_dim=128)
+    test_even_world_size(emb_dim=512)
